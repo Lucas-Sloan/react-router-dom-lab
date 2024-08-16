5b92a527b776abdc701abab16dd8e4a2e70766e5
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-16 12:30:50 -0500

    Created NavBar and corresponding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 // src/App.jsx
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import './App.css'
 
 
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  return (
+    <>
+      <NavBar />  {/* Add NavBar here */}
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        {/* Other routes will be added here */}
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+//src/components/NavBar.jsx
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/mailboxes">Mailboxes</Link>
+        <Link to="/new-mailbox">New Mailbox</Link>
+      </nav>
+    );
+};
+  
+export default NavBar;
