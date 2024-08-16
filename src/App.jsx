8e639bb0c7dc383eb1f38c93ef96f011fa2c1016
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-16 12:52:12 -0500

    Added Mailbox Form and included route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import MailboxForm from './components/MailboxForm';
 import './App.css'
 
 
@@ -9,11 +10,17 @@ import './App.css'
 const App = () => {
   const [mailboxes, setMailboxes] = useState([]);
 
+  const addBox = (newMailboxData) => {
+    newMailboxData._id = mailboxes.length + 1;
+    setMailboxes([...mailboxes, newMailboxData]);
+  };
+
   return (
     <>
-      <NavBar />  {/* Add NavBar here */}
+      <NavBar />
       <Routes>
         <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
         {/* Other routes will be added here */}
       </Routes>
     </>
--- src/components/MailboxForm.jsx
+++ src/components/MailboxForm.jsx
@@ -1,1 +1,25 @@
 //src/components/MailboxForm.jsx
+const MailboxForm = ({ addBox }) => {
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const newMailbox = {
+        boxholder: event.target.boxholder.value,
+        boxSize: event.target.boxSize.value,
+      };
+      addBox(newMailbox);
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input name="boxholder" placeholder="Boxholder Name" required />
+        <select name="boxSize" required>
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+        <button type="submit">Add Mailbox</button>
+      </form>
+    );
+};
+  
+export default MailboxForm;
